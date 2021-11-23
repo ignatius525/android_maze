e67bc3548f1e47c85a911a6e6042175f86fb07bd
Ignat Miagkov <iamiagkov@email.wm.edu>
2021-11-23 15:46:49 -0500

    Added: return to title from WIN

--- app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/WinningFragment.java
+++ app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/WinningFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,5 +27,19 @@ public class WinningFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(WinningFragment.this)
+                        .navigate(R.id.action_winningFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
