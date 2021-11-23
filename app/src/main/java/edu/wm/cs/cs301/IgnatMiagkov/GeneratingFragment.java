fac3bf73d9ec3bbca5f57c82194fc9417f271e47
Ignat Miagkov <iamiagkov@email.wm.edu>
2021-11-22 21:35:27 -0500

    Added: Driver select and progress bar in Generating Frag

--- app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/GeneratingFragment.java
+++ app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/GeneratingFragment.java
@@ -1,9 +1,11 @@
 package edu.wm.cs.cs301.IgnatMiagkov;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,7 +17,11 @@ import edu.wm.cs.cs301.IgnatMiagkov.databinding.FragmentGeneratingBinding;
 
 public class GeneratingFragment extends Fragment {
 
+    private ProgressBar progressBar;
     private FragmentGeneratingBinding binding;
+    private String builder;
+    private Integer diff;
+    Integer count = 1;
 
     @Override
     public View onCreateView(
@@ -35,14 +41,20 @@ public class GeneratingFragment extends Fragment {
             @Override
             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
                 // We use a String here, but any type that can be put in a Bundle is supported
-                String result = bundle.getString("builderKey");
-                Integer diff = bundle.getInt("difficultyKey");
-                TextView textView = getView().findViewById(R.id.textView3);
-                textView.setText(result);
-                TextView textView1 = getView().findViewById(R.id.textView4);
-                textView1.setText(diff.toString());
+                builder = bundle.getString("builderKey");
+                diff = bundle.getInt("difficultyKey");
+//                TextView textView = getView().findViewById(R.id.textView3);
+//                textView.setText(builder);
+//                TextView textView1 = getView().findViewById(R.id.textView4);
+//                textView1.setText(diff.toString());
             }
         });
+
+        progressBar = getView().findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setProgress(0);
+        new MyTask().execute(100);
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +68,33 @@ public class GeneratingFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    // Code refactored from https://www.concretepage.com/android/android-asynctask-example-with-progress-bar for async thread
+    class MyTask extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected String doInBackground(Integer... params) {
+            for (; count <= params[0]; count++) {
+                try {
+                    Thread.sleep(300);
+                    publishProgress(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Task Completed.";
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            progressBar.setVisibility(View.GONE);
+        }
+        @Override
+        protected void onPreExecute() {
+
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+        }
     }
 
 }
