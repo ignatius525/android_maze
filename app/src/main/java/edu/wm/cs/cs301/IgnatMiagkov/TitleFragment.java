a65f18b57e74290a8d48eb09a51403707677c514
Ignat Miagkov <iamiagkov@email.wm.edu>
2021-11-22 20:06:45 -0500

    Added: Explore and Revisit both switch to Generating Fragment

--- app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/TitleFragment.java
+++ app/src/main/java/edu/wm/cs/cs301/IgnatMiagkov/TitleFragment.java
@@ -59,6 +59,14 @@ public class TitleFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        binding.buttonRevisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(TitleFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
     }
 
     @Override
